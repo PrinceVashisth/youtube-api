7d8002263e14eda384e91ea27aac01a552786c06
Prince Vashisth <vashisthprince9@gmail.com>
2023-11-28 17:49:52 +0530

    database link changed

--- databse.js
+++ databse.js
@@ -1,5 +1,5 @@
-// const db = 'mongodb+srv://vashisthprince9:xHAxEgsEm6HEpL4v@cluster0.kvocefs.mongodb.net/?retryWrites=true&w=majority';
-const db = 'mongodb://127.0.0.1:27017/YoutubeClone';
+const db = 'mongodb+srv://vashisthprince9:xHAxEgsEm6HEpL4v@cluster0.kvocefs.mongodb.net/?retryWrites=true&w=majority';
+// const db = 'mongodb://127.0.0.1:27017/YoutubeClone';
 const mongoose = require('mongoose');
 mongoose.connect(db).then(()=>{
     console.log("Database connected");
